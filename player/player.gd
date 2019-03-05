8b4a2c52a88bc073982fce521c509e60aad0dc95
Yeongho Kim <yeonghoey@gmail.com>
2019-03-06 01:18:49 +0900

    Refactor player scene

--- player/player.gd
+++ player/player.gd
@@ -2,22 +2,27 @@ extends "res://mover.gd"
 
 export (int) var force = 60.0
 
+var W = 48
+var H = 72
+
 enum {ACTION_IDLE, ACTION_LEFT, ACTION_RIGHT}
 
 var action = ACTION_IDLE
 var turning = false
 
+
 func _ready():
 	pass
+
 
 func _process(delta):
 	update_action()
 	update_animation()
 
+
 func update_action():
 	var left = Input.is_action_pressed('ui_left')
 	var right = Input.is_action_pressed('ui_right')
-
 	match [action, turning, left, right]:
 		[_, _, false, false]:
 			action = ACTION_IDLE
@@ -47,9 +52,11 @@ func update_animation():
 			$AnimatedSprite.animation = "run"
 			$AnimatedSprite.flip_h = false
 
+
 func _physics_process(delta):
 	update_velocity()
 	move_and_slide(velocity)
+
 
 func _acceleration():
 	match action:
@@ -60,12 +67,14 @@ func _acceleration():
 		ACTION_RIGHT:
 			return Vector2(force, 0)
 
+
 func _friction():
 	match action:
 		ACTION_IDLE:
 			return force
 		_:
 			return 0.0
+
 
 func _max_velocity():
 	return 300.0

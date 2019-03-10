a17b1d5248f5161bf761e07bdf08efad7c47ffc4
Yeongho Kim <yeonghoey@gmail.com>
2019-03-10 19:50:38 +0900

    Update game

--- burning/burning.gd
+++ burning/burning.gd
@@ -1,9 +1,5 @@
 extends AnimatedSprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	play()
 	connect("animation_finished", self, "_on_finished")
--- game/game.gd
+++ game/game.gd
@@ -3,24 +3,20 @@ extends Node
 var WIDTH = 320
 var HEIGHT = 480
 
-
 func _ready():
 	randomize()
 	create_player()
 
-
 func _physics_process(delta):
 	try_spawn_flame()
-
 
 var player_scene = preload("res://player/player.tscn")
 
 func create_player():
 	var player = player_scene.instance()
-	print(player.H)
 	player.position = Vector2(WIDTH/2, HEIGHT-player.H/2)
+	player.connect("hit", self, "_on_hit", [player])
 	add_child(player)
-
 
 var flame_scene = preload("res://flame/flame.tscn")
 var flamespawn_flip = false
@@ -33,7 +29,14 @@ func try_spawn_flame():
 
 	if randf() < flamespawn_threshold:
 		var flame = flame_scene.instance()
-		var x = (WIDTH - flame.W*2) * randf() + flame.W/2
+		var x = (WIDTH - flame.W*2) * randf() + flame.W
 		flame.position = Vector2(x, -flame.H)
 		add_child(flame)
 	flamespawn_threshold *= 1.001;
+
+var burning_scene = preload("res://burning/burning.tscn")
+
+func _on_hit(player):
+	var burning = burning_scene.instance()
+	burning.position = player.position
+	add_child(burning)

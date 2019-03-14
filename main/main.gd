c1555f09fbc984e11e84ea95b369def3c34a6b1f
Yeongho Kim <yeonghoey@gmail.com>
2019-03-15 00:23:37 +0900

    Implement main

---
+++ main/main.gd
@@ -0,0 +1,29 @@
+extends Node
+
+
+func _ready():
+	add_background()
+
+func add_background():
+	var v = get_viewport()
+	var r = v.get_visible_rect()
+	var bg = ColorRect.new()
+	bg.rect_position = r.position
+	bg.rect_size = r.size
+	bg.color = get_color("res://main/palette-background.tres")
+	add_child(bg)
+
+func get_color(res):
+	var palette = load(res)
+	var image = palette.atlas.get_data()
+	image.lock()
+	var pos = palette.region.position
+	var color = image.get_pixel(pos.x, pos.y)
+	image.unlock()
+	return color
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	pass

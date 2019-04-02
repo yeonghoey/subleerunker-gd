4ae4a8dba23bb7241933fdcc1abbf0a02be0e51c
Yeongho Kim <yeonghoey@gmail.com>
2019-04-02 18:51:14 +0900

    Update UI

--- menu/menu.gd
+++ menu/menu.gd
@@ -2,8 +2,8 @@ extends Node
 
 signal pressed
 
-func _process(delta):
-	if (Input.is_action_pressed("ui_left") or
+func _unhandled_input(event):
+	if (Input.is_action_pressed("ui_left") or \
 		Input.is_action_pressed("ui_right")):
 		emit_signal("pressed")
 		queue_free()
--- ui/ui.gd
+++ ui/ui.gd
@@ -1,13 +1,19 @@
-extends Control
+extends MarginContainer
 
-var score := 0
+var current_score_label: Label
+var current_score := 0
 
 func _ready():
-	$CurrentScore.text = str(score)
+	current_score_label = find_node("CurrentScore") as Label
+	reset()
 
 func reset():
-	score = 0
+	current_score = 0
+	update_current_score()
 
 func on_scored():
-	score += 1
-	$CurrentScore.text = str(score)
+	current_score += 1
+	update_current_score()
+
+func update_current_score():
+	current_score_label.text = String(current_score)

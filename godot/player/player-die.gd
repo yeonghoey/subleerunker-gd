9d68ead02e80066be86f6b2497c2cfb449c6da84
Yeongho Kim <yeonghoey@gmail.com>
2019-09-21 07:44:15 +0900

    Use ySpriteAnimator for player-die

--- godot/player/player-die.gd
+++ godot/player/player-die.gd
@@ -1,8 +1,9 @@
-extends AnimatedSprite
+extends Node2D
 
 func _ready():
-	play()
-	connect("animation_finished", self, "_on_finished")
+	$ySpriteAnimator.sprite_pack = load("res://themes/default.tres")
+	$ySpriteAnimator.connect("animation_finished", self, "_on_finished")
+	$ySpriteAnimator.play("player-die")
 
-func _on_finished():
+func _on_finished(anim: String):
 	queue_free()

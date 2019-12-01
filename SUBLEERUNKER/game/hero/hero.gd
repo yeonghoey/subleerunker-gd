48623836b9084ae953cc5c8bc92ae2e679eed63a
Yeongho Kim <yeonghoey@gmail.com>
2019-12-02 06:16:37 +0900

    Make hero.gd resuable

--- SUBLEERUNKER/game/hero/hero.gd
+++ SUBLEERUNKER/game/hero/hero.gd
@@ -1,28 +1,34 @@
 extends Mover
 
-const FORCE := 3600.0
-
 enum {
 	ACTION_REST, 
 	ACTION_LEFT,
 	ACTION_RIGHT,
 }
 
+# These should be Area2D
+export(NodePath) var head_path
+export(NodePath) var feet_path
+
+# AnimationPlayer should contain animations named "idle", "left", "right"
+export(NodePath) var animation_player_path
+
+export(float) var acceleration_size
+export(float) var friction_amount
+export(float) var max_speed
+
+signal action_changed(prev_action, action)
+signal hit()
+signal pedaled()
+
 var _action := ACTION_REST
 # For handling when LR keys are pressed simultaneously
 var _action_overridden := false
 
-
-func _ready():
-	$Head.connect("body_entered", self, "_on_Head_body_entered")
-	$Feet.connect("area_entered", self, "_on_Feet_area_entered")
+onready var _animation_player: AnimationPlayer = get_node(animation_player_path)
 
 
-func _process(delta):
-	update_animation()
-
-
-func update_action(left, right):
+func process_input(left, right):
 	var prev_action := _action
 	match [_action, _action_overridden, left, right]:
 		[_, _, false, false]:
@@ -40,52 +46,59 @@ func update_action(left, right):
 		[ACTION_RIGHT, false, true, true]:
 			_action = ACTION_LEFT
 			_action_overridden = true
-	match [prev_action, _action]:
-		[ACTION_REST, ACTION_LEFT], [ACTION_REST, ACTION_RIGHT]:
-			$AudioRun.play()
-		[ACTION_LEFT, ACTION_REST], [ACTION_RIGHT, ACTION_REST]:
-			$AudioRun.stop()
+	if prev_action != _action:
+		emit_signal("action_changed", prev_action, _action)
+
+
+func _ready():
+	get_node(head_path).connect("body_entered", self, "_on_head_body_entered")
+	get_node(feet_path).connect("area_entered", self, "_on_feet_area_entered")
+
+
+func _on_head_body_entered(body):
+	emit_signal("hit")
+
+
+func _on_feet_area_entered(area):
+	emit_signal("pedaled")
+
+
+func _process(delta):
+	update_animation()
 
 
 func update_animation():
 	match _action:
 		ACTION_REST:
-			$AnimationPlayer.play("player_idle")
+			_animation_player.play("idle")
 		ACTION_LEFT:
-			$AnimationPlayer.play("player_run_left")
+			_animation_player.play("left")
 		ACTION_RIGHT:
-			$AnimationPlayer.play("player_run_right")
+			_animation_player.play("right")
 
 
 func _physics_process(delta: float):
 	move(delta)
 
 
+# Implementatino of Mover virtual methods
 func _acceleration() -> Vector2:
 	match _action:
 		ACTION_LEFT:
-			return Vector2(-FORCE, 0.0)
+			return Vector2(-acceleration_size, 0.0)
 		ACTION_RIGHT:
-			return Vector2(FORCE, 0.0)
+			return Vector2(acceleration_size, 0.0)
 		_:
 			return Vector2(0.0, 0.0)
 
 
-func _friction() -> float:
+func _friction_amount() -> float:
 	match _action:
 		ACTION_REST:
-			return FORCE
+			return friction_amount
 		_:
 			return 0.0
 
 
-func _max_velocity() -> float:
-	return 300.0
-
-
-func _on_Head_body_entered(body):
-	Signals.emit_signal("hit", self)
-
-
-func _on_Feet_area_entered(area):
-	Signals.emit_signal("game_combo_succeeded", area)
+func _max_speed() -> float:
+	return max_speed

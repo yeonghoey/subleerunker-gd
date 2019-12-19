fe580630b740dc3de3e5aea9c70c679df99a4b73
Yeongho Kim <yeonghoey@gmail.com>
2019-12-19 09:57:37 +0900

    Working on Play scene

--- SUBLEERUNKER/main/scene/play.gd
+++ SUBLEERUNKER/main/scene/play.gd
@@ -1,32 +1,66 @@
 extends Control
 
-var _current_mode: String = ""
+const Stadium := preload("res://game/stadium/stadium.gd")
+const Preset := preload("res://game/preset/preset.gd")
 
-onready var _frame: GameFrame = find_node("Frame")
+const ModeSelection := preload("res://game/stage/modeselection.tscn")
+const Leaderboard := preload("res://game/stage/leaderboard.tscn")
+const InGame := preload("res://game/stage/ingame.tscn")
+const ScoreUpload := preload("res://game/stage/scoreupload.tscn")
+
+
+var _current_mode := ""
+var _preset: Preset
+var _myrecord_break := {}
+var _score_old := 0
+
+onready var _Stadium: Stadium = find_node("Stadium")
 
 
 func _ready():
-	_display_modeselection()
+	_present_modeselection()
 
 
-func _display_modeselection():
-	var modeselection := GameView.of("modeselection")
+func _present_modeselection():
+	var modeselection := ModeSelection.instance()
 	modeselection.connect("selected", self, "_on_modeselection_selected")
-	modeselection.connect("canceled", self, "_on_modeselection_canceled")	
-	_frame.display(modeselection)
+	modeselection.connect("canceled", self, "_on_modeselection_canceled")
+	_Stadium.present(modeselection)
 
 
 func _on_modeselection_selected(mode_name: String):
-	_current_mode = mode_name
-	_display_leaderboard()
+	_preset = _load_preset(mode_name)
+	_present_leaderboard()
 
 
 func _on_modeselection_canceled():
 	pass
 
 
-func _display_leaderboard():
-	var leaderboard := GameView.of("leaderboard")
-	var preset := GamePreset.of(_current_mode)
-	leaderboard.init(preset)
-	_frame.display(leaderboard)
+func _present_leaderboard():
+	var leaderboard := Leaderboard.instance()
+	leaderboard.init(_preset, _myrecord_break)
+	leaderboard.connect("started", self, "_on_leaderboard_started")
+	leaderboard.connect("canceled", self, "_on_leaderboard_canceled")
+	_Stadium.present(leaderboard)
+
+
+
+func _on_leaderboard_started(score_old: int):
+	_score_old = score_old
+	_present_ingame()
+
+
+func _on_leaderboard_canceled():
+	pass
+
+
+func _present_ingame():
+	var ingame := InGame.instance()
+	ingame.init(_preset)
+	# TODO: Connect signals!
+	_Stadium.present(ingame)
+
+
+func _load_preset(mode_name: String) -> Preset:
+	return load("res://game/preset/%s.gd" % mode_name).new()

d39a9a6baf0976c15452dc219ee254e8faf302eb
Yeongho Kim <yeonghoey@gmail.com>
2020-01-07 21:18:08 +0900

    Add drop/shower

---
+++ SUBLEERUNKER/drop/shower/shower.gd
@@ -0,0 +1,12 @@
+extends "res://drop/drop.gd"
+
+
+func init(boundary: Vector2, hero: Hero, hint = null) -> void:
+	"""Place the drop in the top random of the boundary.
+	"""
+	var x = (boundary.x - width*2) * randf() + width
+	position = Vector2(x, -height)
+
+
+func _on_Area2D_area_entered(area):
+	queue_free()

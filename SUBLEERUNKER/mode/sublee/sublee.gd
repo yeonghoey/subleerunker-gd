30d45059f020ec4f94b70b14261bf1af26ab6f8f
Yeongho Kim <yeonghoey@gmail.com>
2020-01-13 19:50:26 +0900

    Use flame spawner for sublee

--- SUBLEERUNKER/mode/sublee/sublee.gd
+++ SUBLEERUNKER/mode/sublee/sublee.gd
@@ -8,7 +8,7 @@ func _init().({
 	labelcolor = Color("#ffffff"),
 	Background = preload("res://background/black/black.tscn"),
 	Hero = preload("res://hero/sublee/sublee.tscn"),
-	DropSpawner = preload("res://dropspawner/randframe/redblue.tscn"),
+	DropSpawner = preload("res://dropspawner/randframe/flame.tscn"),
 	PedalSpawner = preload("res://pedalspawner/oneatatime/oneatatime.tscn"),
 	BGM = preload("res://bgm/dddd/dddd.tscn"),
 	Cam = preload("res://cam/randshake/randshake.tscn"),

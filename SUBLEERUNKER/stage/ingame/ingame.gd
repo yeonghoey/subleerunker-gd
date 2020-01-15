51b9496261662a1e1e8e864de4e0a6f747bc37c7
Yeongho Kim <yeonghoey@gmail.com>
2020-01-16 07:00:01 +0900

    Refactor stage/ingame

--- SUBLEERUNKER/stage/ingame/ingame.gd
+++ SUBLEERUNKER/stage/ingame/ingame.gd
@@ -3,13 +3,14 @@ extends "res://stage/stage.gd"
 const Mode := preload("res://mode/mode.gd")
 const Scorer := preload("res://scorer/scorer.gd")
 const Background := preload("res://background/background.gd")
-const Hero := preload("res://hero/hero.gd")
-const Drop := preload("res://drop/drop.gd")
-const DropSpawner := preload("res://dropspawner/dropspawner.gd")
-const Pedal := preload("res://pedal/pedal.gd")
-const PedalSpawner := preload("res://pedalspawner/pedalspawner.gd")
 const BGM := preload("res://bgm/bgm.gd")
 const Cam := preload("res://cam/cam.gd")
+const Drop := preload("res://drop/drop.gd")
+const DropSpawner := preload("res://dropspawner/dropspawner.gd")
+const Hero := preload("res://hero/hero.gd")
+const Pedal := preload("res://pedal/pedal.gd")
+const PedalSpawner := preload("res://pedalspawner/pedalspawner.gd")
+
 const Troupe := preload("troupe.gd")
 
 signal hero_hit(final_score)
@@ -18,13 +19,13 @@ signal ended()
 var _mode: Mode
 var _scorer: Scorer
 
+var _background: Background
 var _bgm: BGM
 var _cam: Cam
-var _background: Background
-var _dropspawner: DropSpawner
-var _pedalspawner: PedalSpawner
 var _troupe: Troupe
 var _hero: Hero
+var _dropspawner: DropSpawner
+var _pedalspawner: PedalSpawner
 
 var _is_hero_hit := false
 
@@ -33,94 +34,64 @@ func init(mode: Mode, scorer: Scorer) -> void:
 	_mode = mode
 	_scorer = scorer
 
-
-func _ready():
-	assert(_mode != null)
-	_add_background()
-	_add_troupe()
-	_cast_hero()
-	_add_bgm()
-	_add_cam()
-	_add_dropspawner()
-	_add_pedalspawner()
-	_wire_events_to_cam()
-	_start()
-
-
-func _add_background():
 	_background = _mode.make("Background")
 	add_child(_background)
 
+	_bgm = _mode.make("BGM")
+	add_child(_bgm)
 
-func _add_troupe():
+	_cam = _mode.make("Cam")
+	add_child(_cam)
+
 	_troupe = Troupe.new()
-	_troupe.connect("cleared", self, "_on_troup_cleared")
 	add_child(_troupe)
 
-
-func _on_troup_cleared():
-	emit_signal("ended")
-
-
-func _cast_hero():
 	_hero = _mode.make("Hero")
 	var starting_pos := _background.hero_starting_pos(_hero.size)
 	_hero.init(starting_pos)
-	_hero.connect("hit", self, "_on_hero_hit")
 	_troupe.cast(_hero)
+
+	_dropspawner = _mode.make("DropSpawner")
+	_dropspawner.init(_scorer, _background, _hero)
+	add_child(_dropspawner)
+
+	_pedalspawner = _mode.make("PedalSpawner")
+	_pedalspawner.init(_scorer, _background, _hero)
+	add_child(_pedalspawner)
+
+
+func _ready():
+	_wire_signals()
+	_start()
+
+
+func _wire_signals() -> void:
+	_dropspawner.connect("cued", self, "_on_spawner_cued")
+	_pedalspawner.connect("cued", self, "_on_spawner_cued")
+	_hero.connect("hit", self, "_on_hero_hit")
+	_troupe.connect("cleared", self, "_on_troup_cleared")
+
+
+func _on_spawner_cued(actors: Array) -> void:
+	for actor in actors:
+		assert((actor is Drop) or (actor is Pedal))
+		_troupe.cast(actor)
 
 
 func _on_hero_hit():
 	_is_hero_hit = true
+
+	_bgm.queue_free()
+	_cam.queue_free()
+	_dropspawner.queue_free()
+	_pedalspawner.queue_free()
+
 	var final_score := _scorer.freeze()
 	emit_signal("hero_hit", final_score)
 
 
-func _add_bgm() -> void:
-	_bgm = _mode.make("BGM")
-	_hero.connect("hit", _bgm, "queue_free")
-	add_child(_bgm)
-
-
-func _add_cam() -> void:
-	_cam = _mode.make("Cam")
-	_hero.connect("hit", _cam, "queue_free")
-	add_child(_cam)
-
-
-func _add_dropspawner():
-	_dropspawner = _mode.make("DropSpawner")
-	_dropspawner.init(_scorer, _background, _hero)
-	_dropspawner.connect("cued", self, "_on_dropspawner_cued")
-	_hero.connect("hit", _dropspawner, "queue_free")
-	add_child(_dropspawner)
-
-
-func _on_dropspawner_cued(drops: Array) -> void:
-	for drop in drops:
-		_troupe.cast(drop)
-
-
-func _add_pedalspawner():
-	_pedalspawner = _mode.make("PedalSpawner")
-	_pedalspawner.init(_scorer, _background, _hero)
-	_pedalspawner.connect("cued", self, "_on_pedalspawner_cued")
-	_hero.connect("hit", _pedalspawner, "queue_free")
-	add_child(_pedalspawner)
-
-
-func _on_pedalspawner_cued(pedals: Array) -> void:
-	for pedal in pedals:
-		_troupe.cast(pedal)
-
-
-func _wire_events_to_cam() -> void:
-	_scorer.connect("initialized", _cam, "on_scorer_initialized")
-	_scorer.connect("scored", _cam, "on_scorer_scored")
-	_scorer.connect("combo_hit", _cam, "on_scorer_combo_hit")
-	_scorer.connect("combo_missed", _cam, "on_scorer_combo_missed")
-	connect("hero_hit", _cam, "on_ingame_hero_hit")
-	connect("ended", _cam, "on_ingame_ended")
+func _on_troup_cleared():
+	emit_signal("ended")
 
 
 func _start() -> void:

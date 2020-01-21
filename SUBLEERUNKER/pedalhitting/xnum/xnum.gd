c6a2c0d59623765d406e21b6e2a2e70c25e24640
Yeongho Kim <yeonghoey@gmail.com>
2020-01-21 22:52:05 +0900

    Add sounds to xnum

--- SUBLEERUNKER/pedalhitting/xnum/xnum.gd
+++ SUBLEERUNKER/pedalhitting/xnum/xnum.gd
@@ -1,7 +1,24 @@
 extends "res://pedalhitting/pedalhitting.gd"
 
+const WaitAll := preload("res://misc/wait_all.gd")
+
+const AUDIO_BY_COMBO := [
+	null,
+	null,
+	preload("xnum_c.wav"),
+	preload("xnum_d.wav"),
+	preload("xnum_e.wav"),
+	preload("xnum_f.wav"),
+	preload("xnum_g.wav"),
+	preload("xnum_a.wav"),
+	preload("xnum_b.wav"),
+]
 
 var _combo: int
+
+onready var _Label: Label = $Label
+onready var _AudioStreamPlayer: AudioStreamPlayer = $AudioStreamPlayer
+onready var _AnimationPlayer: AnimationPlayer = $AnimationPlayer
 
 
 func init(combo: int) -> void:
@@ -9,13 +26,24 @@ func init(combo: int) -> void:
 
 
 func _ready():
-	var label = $Label
-	label.text = "x%d" % _combo
-	# Make it centered
-	var offset_x = label.rect_size.x / 2
-	transform = transform.translated(Vector2(-offset_x, 0))
-	$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
-
-
-func _on_animation_finished(anim_name):
+	_play_label()
+	_play_audio()
+	yield(WaitAll.new([
+		[_AnimationPlayer, "animation_finished"],
+		[_AudioStreamPlayer, "finished"]
+	]), "done")
 	queue_free()
+
+
+func _play_label():
+	_Label.text = "x%d" % _combo
+	# Make it centered
+	var offset_x = _Label.rect_size.x / 2
+	transform = transform.translated(Vector2(-offset_x, 0))
+	_AnimationPlayer.play("default")
+
+
+func _play_audio():
+	var x := _combo if _combo < AUDIO_BY_COMBO.size() else AUDIO_BY_COMBO.size()-1
+	_AudioStreamPlayer.stream = AUDIO_BY_COMBO[x]
+	_AudioStreamPlayer.play()
